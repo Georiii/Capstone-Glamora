b2adcc4867c3431cd0110e3f6828d6557e653188
unknown <Francis>
2025-10-23 22:40:30 +0800

    Add EAS project ID to app.config.js

--- Glamora-App-main/GlamoraApp/app.config.js
+++ Glamora-App-main/GlamoraApp/app.config.js
@@ -4,6 +4,11 @@ module.exports = {
       usesCleartextTraffic: true,  // Allow HTTP traffic for development
       package: "com.glamora.app",
     },
+    extra: {
+      eas: {
+        projectId: "e939fc72-a48a-4a32-bd3b-017e22cdb406"
+      }
+    }
     // ... rest of your expo config
   }
 };
